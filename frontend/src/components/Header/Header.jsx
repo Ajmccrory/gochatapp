78e7555aafd7521a0f02c4039ff0791d87434251
aj <93270610+Ajmccrory@users.noreply.github.com>
2022-03-27 15:23:35 -0400

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,1 +1,10 @@
+import React from "react"; 
+import "./Header.scss";
 
+const Header = () => (
+    <div className="header">
+        <h2>Messenger service</h2>
+    </div>
+);
+
+export default Header;
